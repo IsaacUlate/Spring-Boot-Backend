e914ea49b741471806889c8489a441827c2d868b
IsaacUlate <isaaculate2@gmail.com>
2023-10-25 23:19:55 -0600

    Get-Api

--- backend-shop/src/main/java/com/backend/backendshop/controllers/ProductController.java
+++ backend-shop/src/main/java/com/backend/backendshop/controllers/ProductController.java
@@ -1,8 +1,21 @@
 package com.backend.backendshop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.backend.backendshop.models.entity.Product;
+import java.util.List;
+import com.backend.backendshop.services.ProductService;
 
 @RestController
 public class ProductController {
     
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> list(){
+        return service.findAll();
+    }
+
 }
